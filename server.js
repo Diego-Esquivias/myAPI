6a4af4c909d237ba07eff7211cbb9cf04f025028
Diego-Esquivias <desqui048@west-mec.org>
2024-10-02 14:32:18 -0700

    Update
    -changed the data js file to 2 json files

--- server.js
+++ server.js
@@ -2,98 +2,104 @@
 const express = require('express')
 const app = express()
 const {movies, actors} = require('./db/data')
+const path = require('path');
+const fs = require('fs');
 
 app.use(express.static('public'));
 app.set('view engine', 'html');
 
-
+// Load the index page
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/api/users/admin/:password', (req, res) => {
+// How to get into Admin side
+app.get('/api/admin/:password', (req, res) => {
     console.log(req.params)
     const {password} = req.params
     if(password === '18xm2p1m0pa1'){
-        res.render('admin')
+        console.log("success")
+        res.sendFile(path.join(__dirname, '/public/admin.html'))
     } else {
         res.status(401).json({message: 'Access denied'})
     }
 })
 
-// Return all movies
-app.get('/api/movies', (req, res) => {
-    const newMovies = movies.map(movie =>{
-        const {id, title, releaseYear} = movie
-        return {id, title, releaseYear}
-    })
-    res.json(newMovies)
-})
+// Load movies from JSON file
+const getMovies= () => {
+    const data = fs.readFileSync('./db/movies.json', 'utf8');
+    return JSON.parse(data);
+}
 
-// Return all actors
+const saveMovies = (tasks) => {
+    fs.writeFileSync('./db/movies.json', JSON.stringify(tasks, null, 2));
+};
+
+// Load actors from JSON file
+const getActors = () => {
+    const data = fs.readFileSync('./db/actors.json', 'utf8');
+    return JSON.parse(data);
+}
+
+const saveActors = (events) => {
+    fs.writeFileSync('./db/actors.json', JSON.stringify(events, null, 2));
+};
+
+// Routes
+
+// GET: Show all movies
+app.get('/api/movies', (req, res) => {
+    const movies = getMovies();
+    res.json(movies)
+});
+
+// GET: Show all actors
 app.get('/api/actors', (req, res) => {
-    const newActors = actors.map(actor =>{
-        const {id, name, age, movie_id} = actor
-        return {id, name, age, movie_id}
-    })
-    res.json(newActors)
-})
+    const actors = getActors();
+    res.json(actors)
+});
 
 
 // This is how you set up your params for the data query 
 // Find a specific movie based on the id
 app.get('/api/movies/:movieID', (req, res) => {
-    console.log(req.query)
+    const movies = getMovies();
     const {movieID} = req.params
     const singleMovie = movies.find((movie) => movie.id === Number(movieID))
     
     if(!singleMovie){
         return res.status(404).send('Movie not found/item does not exist')
-    } else if (req.query.Life == '42') {
-        return res.status(202).send("You have found the easter egg")
-    }
+    } 
     return res.json(singleMovie)
 })
 
 // Find a specific actor based on the id
 app.get('/api/actors/:actorID', (req, res) => {
-    console.log(req.query)
+    const actors = getActors();
     const {actorID} = req.params
     const singleActor = actors.find((actor) => actor.id === Number(actorID))
     
     if(!singleActor){
         return res.status(404).send('actor not found/item does not exist')
-    } else if (req.query.Life == '42') { // type "?Life=42" in the url
-        return res.status(202).send("You have found the easter egg")
-    }
+    } 
+    // else if (req.query.Life == '42') { // type "?Life=42" in the url
+    //     return res.status(202).send("You have found the easter egg")
+    // }
     return res.json(singleActor)
 })
 
-// app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-//     console.log(req.params)
-//     console.log(Number(req.params.productID))
-//     res.send("This product has been reviewed by a person: its the best there is 10/10 would buy again")
-// })
-
-// // Sets up a query that you can grab
-// app.get('/api/vi/query', (req, res) => {
-//     // console.log(req.query)
-//     const {search, limit} = req.query
-//     let sortedProducts = [...products]
-
-//     if(search) {
-//         sortedProducts = sortedProducts.filter((product) => {
-//             return product.name.startsWith(search)
-//         })
-//     }
-//     if(limit) {
-//         sortedProducts = sortedProducts.slice(0, Number(limit))
-//     }
-//     if(sortedProducts.length < 1) {
-//         return res.status(200).json({success:true, data:[]})
-//     }
-//     res.status(200).json(sortedProducts)
-// })
+// POST: Add new movie
+app.post('/movies', (req, res) => {
+    const movies = getMovies();
+        const newMovie = {
+            id: movies.length + 1, // TODO: edit this to function properly ----------------------------------------------------------------
+            title: req.body.title,
+            releaseYear: req.body.releaseYear
+        };
+        movies.push(newMovie);
+        saveMovies(movies);
+        res.redirect('/');
+})
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000...')
